47ea341b17ac1f6fb422a95ea32f35b8d419b6ca
temandr <brentwoodpav@gmail.com>
2014-10-18 10:34:16 -0400

    working copy

--- ironman-data/ironman-data/main.cpp
+++ ironman-data/ironman-data/main.cpp
@@ -11,19 +11,26 @@
 #include <string>
 #include <iomanip>
 #include <stdexcept>
+#include <vector>
+#include <string>
 
 // Including Myo SDK
 #include <myo/myo.hpp>
 
-// Classes that inherit from myo::DeviceListener can be used to receive events from Myo devices. DeviceListener
-// provides several virtual functions for handling different kinds of events. If you do not override an event, the
-// default behavior is to do nothing.
+
 class DataCollector : public myo::DeviceListener {
 public:
+    /*
     DataCollector()
     : onArm(false), roll_w(0), pitch_w(0), yaw_w(0), currentPose()
     {
     }
+     */
+    
+    void onPair(myo::Myo* myo, uint64_t timestamp, myo::FirmwareVersion firmwareVersion){
+        knownMyos.push_back(myo);
+    }
+
     
     // onUnpair() is called whenever the Myo is disconnected from Myo Connect by the user.
     void onUnpair(myo::Myo* myo, uint64_t timestamp)
@@ -33,7 +40,21 @@ public:
         roll_w = 0;
         pitch_w = 0;
         yaw_w = 0;
-        onArm = false;
+        //onArm = false;
+    }
+    
+    void onConnect(myo::Myo* myo, uint64_t timestamp, myo::FirmwareVersion firmwareVersion)
+    {
+        using std::cout;
+        using std::endl;
+        cout << "Myo " << identifyMyo(myo) << " has connected." << endl;
+    }
+    
+    void onDisconnect(myo::Myo* myo, uint64_t timestamp)
+    {
+        using std::cout;
+        using std::endl;
+        cout << "Myo " << identifyMyo(myo) << " has disconnected." << endl;
     }
     
     // onOrientationData() is called whenever the Myo device provides its current orientation, which is represented
@@ -45,6 +66,7 @@ public:
         using std::sqrt;
         using std::max;
         using std::min;
+        using std::string;
         
         // Calculate Euler angles (roll, pitch, and yaw) from the unit quaternion.
         float roll = atan2(2.0f * (quat.w() * quat.x() + quat.y() * quat.z()),
@@ -57,41 +79,64 @@ public:
         roll_w = static_cast<int>((roll + (float)M_PI)/(M_PI * 2.0f) * 18);
         pitch_w = static_cast<int>((pitch + (float)M_PI/2.0f)/M_PI * 18);
         yaw_w = static_cast<int>((yaw + (float)M_PI)/(M_PI * 2.0f) * 18);
+        
+        string str = std::to_string(roll_w) + " " + std::to_string(pitch_w) + " " + std::to_string(yaw_w);
+        print(myo, str);
+        
     }
     
     // onPose() is called whenever the Myo detects that the person wearing it has changed their pose, for example,
     // making a fist, or not making a fist anymore.
     void onPose(myo::Myo* myo, uint64_t timestamp, myo::Pose pose)
     {
+        /*
+        using std::cout;
+        using std::endl;
+        using std::string;
+        using std::flush;
+         */
+        using std::string;
+        
         currentPose = pose;
         
         // Vibrate the Myo whenever we've detected that the user has made a fist.
         if (pose == myo::Pose::fist) {
             myo->vibrate(myo::Myo::vibrationMedium);
         }
+        
+        //cout << identifyMyo(myo) << " " << roll_w << " " << pitch_w << " " << yaw_w << " ";
+        //cout << pose.toString() << endl;
+        
+        //Old string format
+        //cout << (whichArm == myo::armLeft ? "L " : "R ") << roll_w << " " << pitch_w << " " << yaw_w << " ";
+        //cout << flush;
+        if(identifyMyo(myo) == 'L') {
+            
+        } else {
+            //This is the right myo
+        }
+        //print(myo, pose.toString());
+        
     }
     
     // onArmRecognized() is called whenever Myo has recognized a Sync Gesture after someone has put it on their
     // arm. This lets Myo know which arm it's on and which way it's facing.
     void onArmRecognized(myo::Myo* myo, uint64_t timestamp, myo::Arm arm, myo::XDirection xDirection)
     {
-        onArm = true;
-        whichArm = arm;
+        //onArm = true;
+        //whichArm = arm;
     }
     
-    // onArmLost() is called whenever Myo has detected that it was moved from a stable position on a person's arm after
-    // it recognized the arm. Typically this happens when someone takes Myo off of their arm, but it can also happen
-    // when Myo is moved around on the arm.
     void onArmLost(myo::Myo* myo, uint64_t timestamp)
     {
-        onArm = false;
+        //onArm = false;
     }
     
     // There are other virtual functions in DeviceListener that we could override here, like onAccelerometerData().
     // For this example, the functions overridden above are sufficient.
     
     // We define this function to print the current values that were updated by the on...() functions above.
-    void print()
+    void print(myo::Myo* myo, std::string str)
     {
         using std::cout;
         using std::endl;
@@ -99,14 +144,12 @@ public:
         using std::flush;
         
         // Print out the orientation. Orientation data is always available, even if no arm is currently recognized.
-        cout << (whichArm == myo::armLeft ? "L " : "R ") << roll_w << " " << pitch_w << " " << yaw_w << " ";
         
+        //cout << "Myo " << identifyMyo(
+        
+        /*
         if (onArm) {
-            // Print out the currently recognized pose and which arm Myo is being worn on.
-            
-            // Pose::toString() provides the human-readable name of a pose. We can also output a Pose directly to an
-            // output stream (e.g. std::cout << currentPose;). In this case we want to get the pose name's length so
-            // that we can fill the rest of the field with spaces below, so we obtain it as a string using toString().
+
             string poseString = currentPose.toString();
             
             cout << poseString << endl;
@@ -114,17 +157,33 @@ public:
             // Print out a placeholder for the arm and pose when Myo doesn't currently know which arm it's on.
             cout << "[?]" << endl;
         }
+         
+         cout << identifyMyo(myo) << " " << roll_w << " " << pitch_w << " " << yaw_w << " ";
+         cout << pose.toString() << endl;
+         */
+        cout << identifyMyo(myo) << " " << str << endl;
         
-        cout << flush;
+        //cout << flush;
+    }
+    
+    char identifyMyo(myo::Myo * myo) {
+        for(size_t i =0; i<knownMyos.size(); i++){
+            if(knownMyos[i]==myo){
+                return (i+1) == 2 ? 'R':'L';
+            }
+        }
+        return 0;
     }
     
     // These values are set by onArmRecognized() and onArmLost() above.
-    bool onArm;
-    myo::Arm whichArm;
+    //bool onArm;
+    //myo::Arm whichArm;
     
     // These values are set by onOrientationData() and onPose() above.
     int roll_w, pitch_w, yaw_w;
     myo::Pose currentPose;
+    std::vector<myo::Myo*> knownMyos;
+    string m1, m2;
 };
 
 int main(int argc, const char * argv[])
@@ -132,8 +191,6 @@ int main(int argc, const char * argv[])
     // We catch any exceptions that might occur below -- see the catch statement for more details.
     try {
         
-        // First, we create a Hub with our application identifier. Be sure not to use the com.example namespace when
-        // publishing your application. The Hub provides access to one or more Myos.
         myo::Hub hub("com.aaad.ironman-data");
         
         std::cerr << "Attempting to find a Myo..." << std::endl;
@@ -142,12 +199,14 @@ int main(int argc, const char * argv[])
         // immediately.
         // waitForAnyMyo() takes a timeout value in milliseconds. In this case we will try to find a Myo for 10 seconds, and
         // if that fails, the function will return a null pointer.
-        myo::Myo* myo = hub.waitForMyo(10000);
+        //myo::Myo* myo = hub.waitForMyo(10000);
         
         // If waitForAnyMyo() returned a null pointer, we failed to find a Myo, so exit with an error message.
+        /*
         if (!myo) {
             throw std::runtime_error("Unable to find a Myo!");
         }
+         */
         
         // We've found a Myo.
         std::cerr << "Connected to a Myo armband!" << std::endl << std::endl;
@@ -166,7 +225,7 @@ int main(int argc, const char * argv[])
             hub.run(1000/20);
             // After processing events, we call the print() member function we defined above to print out the values we've
             // obtained from any events that have occurred.
-            collector.print();
+            //collector.print();
         }
         
         // If a standard exception occurred, we print out its message and exit.
