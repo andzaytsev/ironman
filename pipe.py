a8aebdc35e3b8b5037dbdb2b3f2c4233785fd452
Andrey Zaytsev <zaytsev2@illinois.edu>
2014-10-18 06:54:29 -0400

    Fixed pipes and redirecting stdout

--- pipe.py
+++ pipe.py
@@ -1,16 +1,7 @@
 import os
 import random
 import time
-import subprocess
 import sys
-
-cmd = ["./d"]
-
-def close():
-	# os.close(pipeout)
-	# os.close(pipein)
-	pass
-
 
 def readLine(pipeout):
 	return pipeout.readline()
@@ -20,25 +11,11 @@ def writeLine(pipein):
 	os.write(pipeout, str(random.randint(0, 100))+"\n")
 
 if __name__ == '__main__':
-	pipein, pipeout = os.pipe()
-	# if os.fork() == 0:
-	# 	# subprocess.Popen(cmd, stderr = pipeout)
-	# 	for i in xrange(0, 100):
-	# 		writeLine(pipein)
-	# 	close()
-	# else:
-	# 	pipeout = os.fdopen(pipeout)
-	# 	for i in xrange(0, 100):
-	# 		#pass
-	# 		print readLine(pipeout)
-	# 	close()
-	p = subprocess.Popen(cmd, shell=True ,stdout=subprocess.PIPE)
-	# Send input to p.
-	# Now start grabbing output.
-	stdout = []
-	while True:
-		line = p.stdout.readline()
-		stdout.append(line)
-		print line[:-1]
-		if line == '' and p.poll() != None:
-			break
+	pipeout, pipein = os.pipe()
+        old_stdout = sys.stdout
+        sys.stdout = os.fdopen(pipein, 'w')
+	if os.fork() == 0: # child
+                print 'hello'
+        else: # parent
+	 	pipeout = os.fdopen(pipeout, 'r')
+                old_stdout.write(readLine(pipeout))
