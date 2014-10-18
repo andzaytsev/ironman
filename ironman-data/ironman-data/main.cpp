c9cb36e5a3b49e2fafc3b9dd717ca210a9df131e
Andrey Zaytsev <zaytsev2@illinois.edu>
2014-10-18 04:27:30 -0400

    Created a command-line XCode project

---
+++ ironman-data/ironman-data/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ironman-data
+//
+//  Created by Andrey Zaytsev on 10/18/14.
+//  Copyright (c) 2014 AAAD. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
